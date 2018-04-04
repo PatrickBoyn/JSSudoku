d0eae2d9f19bf8d9502f79f4ddece224981b6387
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-03 22:30:30 -0400

    Added a border to see the paragraphs. Also added a join to get rid of the commas.

--- Sudoku.js
+++ Sudoku.js
@@ -3,6 +3,6 @@ let p = document.querySelector(".number");
 const arr = [1,2,3,4,5,6,7,8,9];
 
 for(var i = 0; i <= arr.length; i++){
-   p.textContent = arr.toString();
+   p.textContent = arr.join(" ").toString();
    console.log(i);
 }
