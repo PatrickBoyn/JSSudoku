8f1d772058c422fb42d6d8223f94331adadf821e
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-04 01:01:29 -0400

    Added r where i was. This is one step closer to randomizing the numbers.

--- Sudoku.js
+++ Sudoku.js
@@ -3,4 +3,4 @@ let r = Math.floor(Math.random() * 10);
 // I can't claim credit for this lambda expression,
 // You have to admit it's pretty elegant. My bit is the + 1
 // Sudoku has 9 numbers 1-9
-p.forEach((p, i) => p.textContent=i+1);
+p.forEach((p, i) => p.textContent=r+1);
