b370de79f154a4a97e55ad87347071d0c843fcba
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-04 17:21:46 -0400

    Cool effect, but not what I am after either. I'm sure the solution is adding Something like this.

--- Sudoku.js
+++ Sudoku.js
@@ -8,7 +8,7 @@ let r = Math.floor(Math.random() * arr.length);
    You have to admit it's pretty elegant. My bit is the + 1
    Sudoku has 9 numbers 1-9 +=1
    The arrow click function is mine however. */
-button.addEventListener("click", () => { p.forEach((p, i) => p.textContent=r += 1);});
+button.addEventListener("click", () => { p.forEach((p, i) => p.textContent +=r);});
 
 // So I can see if there is a problem.
 console.log(arr);
