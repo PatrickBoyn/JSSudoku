bc552e3fafe082945579791facb16fa447957ad2
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-03 22:35:25 -0400

    Deleted the =.

--- Sudoku.js
+++ Sudoku.js
@@ -1,8 +1,8 @@
-let p = document.querySelector(".number");
+let p = document.querySelectorAll(".number");
 
 const arr = [1,2,3,4,5,6,7,8,9];
 
-for(var i = 0; i <= arr.length; i++){
+for(var i = 0; i < arr.length; i++){
    p.textContent = arr.join(" ").toString();
    console.log(i);
 }
