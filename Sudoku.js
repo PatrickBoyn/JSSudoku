d18450bcd976f8ddc9745d79f18e18b984f8596b
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-04 17:14:00 -0400

    Added a button listener.

--- Sudoku.js
+++ Sudoku.js
@@ -1,10 +1,15 @@
 let p = document.querySelectorAll(".number");
+let button = document.querySelector("button");
 
 let arr = [1,2,3,4,5,6,7,8,9];
 let r = Math.floor(Math.random() * arr.length);
+
 // I can't claim credit for this lambda expression,
 // You have to admit it's pretty elegant. My bit is the + 1
 // Sudoku has 9 numbers 1-9 +=1
-p.forEach((p, arr) => p.textContent=r + 1);
+// The arrow click function is mine however. 
+button.addEventListener("click", () => { p.forEach((p, arr) => p.textContent=r + 1);});
+
+
 
 console.log(arr);
