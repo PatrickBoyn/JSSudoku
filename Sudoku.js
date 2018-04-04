2abbe66ef3d1b1c779ff91925f091c5ffd0ad7bf
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-04 18:38:39 -0400

    Working on how to make every number random.

--- Sudoku.js
+++ Sudoku.js
@@ -2,7 +2,7 @@ let p = document.querySelectorAll(".number");
 let button = document.querySelector("button");
 
 let arr = [1,2,3,4,5,6,7,8,9];
-let r = Math.floor(Math.random() * arr.length);
+let r = Math.floor(Math.random() * 9);
 
 randomText();
 
