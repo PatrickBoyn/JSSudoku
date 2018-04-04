843075783f416eccb004dac6a1a92090fcc1ad12
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-04 01:07:07 -0400

    Playing around with ways of only getting between 1-9.

--- Sudoku.js
+++ Sudoku.js
@@ -1,6 +1,6 @@
 let p = document.querySelectorAll(".number");
-let r = Math.floor(Math.random() * 10);
+let r = Math.floor(Math.random() * 9);
 // I can't claim credit for this lambda expression,
 // You have to admit it's pretty elegant. My bit is the + 1
 // Sudoku has 9 numbers 1-9
-p.forEach((p, i) => p.textContent=r+1);
+p.forEach((p, i) => p.textContent=r += 1);
