3da02307c01c46315aeeb8df00bdee432183b952
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-04 01:09:43 -0400

    Tried something but decided against it.

--- Sudoku.js
+++ Sudoku.js
@@ -1,6 +1,8 @@
 let p = document.querySelectorAll(".number");
 let r = Math.floor(Math.random() * 9);
+
 // I can't claim credit for this lambda expression,
 // You have to admit it's pretty elegant. My bit is the + 1
 // Sudoku has 9 numbers 1-9
 p.forEach((p, i) => p.textContent=r += 1);
+
