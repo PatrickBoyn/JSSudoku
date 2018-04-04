650afb7d9b6e38ee69f4b5053483d9ea9eb819a7
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-04 18:50:20 -0400

    This just give me the same problem as before.

--- Sudoku.js
+++ Sudoku.js
@@ -14,7 +14,8 @@ button.addEventListener("click", () => {randomText();});
 
 // I will want to use this again maybe. 
 function randomText(){
-    p.forEach((p, i) => p.textContent = r);
+        p.forEach((p, i) => p.textContent = i+=r);
+        console.log(r);
 };
 
 // So I can see if there is a problem.
