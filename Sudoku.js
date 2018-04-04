e14d4e1293e622b7e4dc9235e1f7d5339aa4208d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-04 00:54:48 -0400

    I know it's probably not the right way of using flex-box, but I'm just getting back into using it.

--- Sudoku.js
+++ Sudoku.js
@@ -2,5 +2,5 @@ let p = document.querySelectorAll(".number");
 
 // I can't claim credit for this lambda expression,
 // You have to admit it's pretty elegant. My bit is the + 1
-// Sudoku has 9 numbers 1-9. 
+// Sudoku has 9 numbers 1-9
 p.forEach((p, i) => p.textContent=i+1);
