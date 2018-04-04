7617478cb2d345fd50c9d48c410aeac9324d44a6
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-04 18:07:34 -0400

    Some refactoring.

--- Sudoku.js
+++ Sudoku.js
@@ -2,13 +2,13 @@ let p = document.querySelectorAll(".number");
 let button = document.querySelector("button");
 
 let arr = [1,2,3,4,5,6,7,8,9];
-let r = Math.floor(Math.random());
+let r = Math.floor(Math.random() * arr.length + 1);
 
 /* II can't claim credit for this lambda expression,
    You have to admit it's pretty elegant. My bit is the + 1
    Sudoku has 9 numbers 1-9 +=1
    The arrow click function is mine however. */
-button.addEventListener("click", () => { p.forEach((p, i) => p.textContent = r += 1);});
+button.addEventListener("click", () => { p.forEach((p, i) => p.textContent = r);});
 
 // So I can see if there is a problem.
 console.log(arr);
