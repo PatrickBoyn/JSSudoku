753a26babbd5c042e1c9ca584bdeb7cabdcd3c14
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-04 17:40:01 -0400

    Went back to old code I knew at least kind of worked.

--- Sudoku.js
+++ Sudoku.js
@@ -8,7 +8,7 @@ let r = Math.floor(Math.random());
    You have to admit it's pretty elegant. My bit is the + 1
    Sudoku has 9 numbers 1-9 +=1
    The arrow click function is mine however. */
-button.addEventListener("click", (r) => { p.forEach((p, i) => p.textContent = r += 1);});
+button.addEventListener("click", () => { p.forEach((p, i) => p.textContent = r += 1);});
 
 // So I can see if there is a problem.
 console.log(arr);
