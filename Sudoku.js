4c0d8f9be81a1d585af8032e646eef4a74595d63
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-05 18:32:11 -0400

    Added a console log to find out why once in awhile it would not have a correct square.

--- Sudoku.js
+++ Sudoku.js
@@ -13,13 +13,20 @@ let j = Math.floor(Math.random() * 9) + 1;
 
 let correct = p[a];
 
+console.log(p[a]);
 fillNumbers();
 /* II can't claim credit for this lambda expression,
    You have to admit it's pretty elegant. My bit is the + 1
    Sudoku has 9 numbers 1-9
    The arrow click function is mine however. */
 button.addEventListener("click", () => {fillNumbers();});
-correct.style.background = "green";
+if( correct > 1){
+    correct.style.background = "green";
+} else{
+    correct = p[b];
+    correct.style.background = "green";
+}
+
 // I will want to use this again maybe. 
 function fillNumbers(){
         // The += is so that the numbers will change across all squares.   
@@ -35,4 +42,3 @@ function fillNumbers(){
         p[7].textContent = h;
         p[8].textContent = j;
 };
-        console.log(r)
