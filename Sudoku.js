f1bd3bfeefc2f39bc1aa7331e007d8034ab23bc2
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-04 21:00:38 -0400

    Went back to 1-9. At least I have this down now.

--- Sudoku.js
+++ Sudoku.js
@@ -1,18 +1,18 @@
 let p = document.querySelectorAll(".number");
 let button = document.querySelector("button");
 
-let r = Math.floor(Math.random() * 9) + 1;
+let r = Math.floor(Math.random() * 9);
 
 randomText();
 
 /* II can't claim credit for this lambda expression,
    You have to admit it's pretty elegant. My bit is the + 1
-   Sudoku has 9 numbers 1-9 +=1
+   Sudoku has 9 numbers 1-9
    The arrow click function is mine however. */
 button.addEventListener("click", () => {randomText();});
 
 // I will want to use this again maybe. 
 function randomText(){
-        p.forEach((p, i) => p.textContent = i+=r);
+        p.forEach((p, i) => p.textContent = i + 1);
         console.log(r);
 };
