2ad5798d59998ba92685606796b436b861ec142e
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-12 20:11:24 -0400

    Something about changing it via js makes the squares not work.

--- Sudoku.js
+++ Sudoku.js
@@ -16,7 +16,7 @@ let j = Math.floor(Math.random() * 9) + 1;
 // I will figure out a way of making it pick a new one.
 let correct = p[a];
 
-same.textContent = correct;
+same.textContent = "Testing 1,2,3";
  console.log(test);
 if(correct === p[a]){
     console.log(correct)
