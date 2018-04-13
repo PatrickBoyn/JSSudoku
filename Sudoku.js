1222500547c0e403f47ecd57b1e9d485de01e2dc
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-12 22:29:54 -0400

    Tried yet another thing to get the text to display more than object type.  Also added the places for the numbers you will use to pick.

--- Sudoku.js
+++ Sudoku.js
@@ -16,7 +16,7 @@ let j = Math.floor(Math.random() * 9) + 1;
 // I will figure out a way of making it pick a new one.
 let correct = p[a];
 
-same.innerHTML = correct;
+same.innerText = correct;
  
 if(correct === p[a]){
     console.log(correct)
